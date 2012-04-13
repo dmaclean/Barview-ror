15cbb8ee194f07e067f4cfd866524b1189fbaed4
Dan MacLean <dmaclean82@gmail.com>
2012-04-12 20:56:49 -0400

    Added baseline project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarviewRor::Application.config.secret_token = '33bb93eeec38a2cc34f1b848635b03c1d50b763ab2f1c393bb7120ab98b68bacbb9a6e9340911e1635e1fbabe6581e6e4144fbaba62471578d05f8fc0136088b'
