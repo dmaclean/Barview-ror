45e44b8ee9b40f71ecc84426a8121dea58e72584
Dan MacLean <dmaclean82@gmail.com>
2012-04-16 16:18:12 -0400

    Configured the barhome controller to identify logged-in users and set a flag.  On the view side we display different content for logged-in vs non-logged-in users.  I also added functional tests for the controller.

--- app/controllers/barhome_controller.rb
+++ app/controllers/barhome_controller.rb
@@ -1,4 +1,11 @@
 class BarhomeController < ApplicationController
   def index
+    @logged_in = session[:bar_id] != nil
+    #@r = Random.new
+    
+    # User is logged in
+    if @logged_in
+      @bar = Bar.find(session[:bar_id])
+    end
   end
 end
--- test/functional/barhome_controller_test.rb
+++ test/functional/barhome_controller_test.rb
@@ -6,4 +6,19 @@ class BarhomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "bar is not logged in" do
+    get :index
+    assert_select '#welcome h1', 'Welcome to Bar-view.com.'
+	assert_select '#webcam_desc', :minimum => 1
+	assert_select '#signup_desc', :minimum => 1
+  end
+  
+  test "bar is logged in" do
+    session[:bar_id] = 1
+    get :index
+    assert_select '#realtime', :minimum => 1
+    assert_select '#broadcast', :minimum => 1
+    assert_select '#bar_events_list', :minimum => 1
+    assert_select '.bar_events_edit', :minimum => 1
+  end
 end
