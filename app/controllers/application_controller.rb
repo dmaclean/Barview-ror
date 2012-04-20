9398b1e840e002c4b5442f9493d65154a0ac96da
Dan MacLean <dmaclean82@gmail.com>
2012-04-20 12:45:37 -0400

    Moved the header and footer stuff to the application html so we don't have to change it in two places between the user and bar sites.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :init_vars
+  
+  private
+  def init_vars
+    @base_url = 'http://' + request.host
+    if request.port != 80
+      @base_url += ':' + request.port.to_s
+    end
+  end
 end
