55885abccfd639a4381dcd371970cb384e393bd4
Dan MacLean <dmaclean82@gmail.com>
2012-04-27 06:47:22 -0400

    Added the user_home controller and fixed the layout so it looks like real bar-view.com.

--- app/controllers/user_home_controller.rb
+++ app/controllers/user_home_controller.rb
@@ -1,4 +1,56 @@
 class UserHomeController < ApplicationController
   def index
+    # Check if we're dealing with a bar.  If so, redirect them to /barhome
+	if session[:bar_id]
+	  redirect_to barhome_url, :flash => { :notice => "If you would like to view the main Barview page, log out first, then click the 'Users' link." }
+	  return
+	end
+    
+    # We have a logged-in user
+    if session[:user_id]
+      @bars = Bar.joins(:favorites).where('favorites.user_id' => session[:user_id])
+      
+      if @bars.empty? 
+        @bars = Bar.find(:all)
+        @events = get_all_events
+      else
+        @nonfaves = Bar.all(:limit => 5 - @bars.length, :conditions => ['id not in (select bar_id from favorites where user_id = ?)', [session[:user_id]] ])
+        @nonfave_events = Bar.all(:select => 'bars.name, bar_events.detail', :joins => :bar_event)
+		@events = get_events_for_favorites
+        @has_favorites = true
+      end
+    else
+      @bars = Bar.find(:all)
+      @events = get_all_events
+    end
+  end
+  
+  private
+  def get_events_for_favorites
+    events = Hash.new
+    temp = Bar.find(:all, :select => 'bars.name, bar_events.detail', :joins => :bar_event, :conditions => ['id in (select bar_id from favorites where user_id = ?)', [session[:user_id]] ])
+    for t in temp do
+	  if events.key?(t.name)
+	    events[t.name] << t.detail
+	  else
+	    events[t.name] = [t.detail]
+	  end
+	end
+	
+	events
+  end
+  
+  def get_all_events
+    events = Hash.new
+	temp = Bar.find(:all, :select => 'bars.name, bar_events.detail', :joins => :bar_event)
+	for t in temp do
+	  if events.key?(t.name)
+	    events[t.name] << t.detail
+	  else
+	    events[t.name] = [t.detail]
+	  end
+	end
+	
+	events
   end
 end
--- test/functional/user_home_controller_test.rb
+++ test/functional/user_home_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 class UserHomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get index user with favorites" do
+    session[:user_id ] = 1
     get :index
+    assert_select '#fave_h1', 'Your Favorites'
+    assert_select '#feeds_h1', 'Feeds from your favorites'
+    assert_select '#fave_col' do |cols|
+      assert_select 'ul' do 
+        assert_select 'li', 1
+      end
+    end
     assert_response :success
+  end
+  
+  test "should get index user without favorites" do
+    session[:user_id ] = 3
+    get :index
+    assert_select '#fave_h1', 'Bar-view Bars'
+    assert_select '#feeds_h1', 'Current Feeds'
+    assert_response :success
+  end
+  
+  test "bar user is lost" do
+    session[:bar_id] = 1
+    get :index
+    assert_redirected_to barhome_url
   end
 
 end
