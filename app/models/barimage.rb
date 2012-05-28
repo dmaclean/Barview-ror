e9c57f727d46c282a306925f911f4c34b5715892
Dan MacLean <dmaclean82@gmail.com>
2012-05-28 18:03:56 +0200

    Changed image fetching mechanism to use S3 instead of local storage.

--- app/models/barimage.rb
+++ app/models/barimage.rb
@@ -1,28 +1,43 @@
+require 'aws/s3'
+
 class Barimage < ActiveRecord::Base
-  attr_accessible :image
+  attr_accessible :image, :bar_id
   
   belongs_to :bar
   
   def read_file
-    file = File.new(self.image, 'r')
-    if file.exist?
-      @img_binary = file.read
-      file.close
+    s3init
+    
+    if AWS::S3::S3Object.exists? self.image, @bucketname
+      @img_binary = AWS::S3::S3Object.value self.image, @bucketname
     else
-      file = File.new('public/broadcast_images/barview.jpg', 'r')
-      @img_binary = file.read
-      file.close
+      @img_binary = AWS::S3::S3Object.value 'barview.jpg', @bucketname
     end
     
     @img_binary
   end
   
   def write_file(data)
-    file = File.open('public/' + self.image, 'w')
-    file.puts(data)
-	file.close
+    s3init
+    
+    return AWS::S3::S3Object.store(self.image, data, @bucketname)
   end
   
   private
-  
+  def s3init
+    if not AWS::S3::Base.connected?
+      AWS::S3::Base.establish_connection!(
+        :access_key_id     => 'AKIAJJLWIEOFCWR5T2KQ',
+        :secret_access_key => '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'
+      )
+    end
+    
+    if Rails.env.development?
+      @bucketname = 'barview_dev'
+    elsif Rails.env.test?
+      @bucketname = 'barview_test'
+    elsif Rails.env.production?
+      @bucketname = 'barview'
+    end
+  end
 end
--- test/unit/barimage_test.rb
+++ test/unit/barimage_test.rb
@@ -1,17 +1,38 @@
 require 'test_helper'
 
 class BarimageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
   test "save image works" do
     bar_image = Barimage.new
-    bar_image[:bar_id] = 2
-    bar_image[:image] = 'broadcast_images/2.jpeg'
-    bar_image.save
+    bar_image.id = 3
+    bar_image.bar_id = 3
+    bar_image.image = '3.jpg'
+    
+    flunk( "Image was not saved correctly" ) unless bar_image.save
+    
+    bar_image = Barimage.find_by_bar_id(3)
+    assert bar_image.bar_id == 3
+    assert bar_image.image == '3.jpg'
+  end
+  
+  test "read image binary" do
     bar_image = Barimage.find_by_bar_id(2)
-    assert bar_image[:bar_id] == 2
-    assert bar_image[:image] == 'broadcast_images/2.jpeg'
+    binary = bar_image.read_file
+    assert binary != nil and binary != ''
+  end
+  
+  test "read image binary for non-existent image" do
+    bar_image = Barimage.find_by_bar_id(1)
+    binary = bar_image.read_file
+    assert binary != nil and binary != ''
+  end
+  
+  test "write image binary" do
+    bar_image = Barimage.new
+    bar_image.bar_id = 3
+    bar_image.image = '3.jpg'
+    binary = open('public/broadcast_images/barview.jpg','r').read
+    flunk("writing file to S3 failed") unless bar_image.write_file(binary)
+    binary = bar_image.read_file
+    assert binary != nil and binary != ''
   end
 end
