871d284e2d3739e668a78ff1bfdf8702f77d55e9
Dan MacLean <dmaclean82@gmail.com>
2012-05-30 12:13:50 -0400

    Added facebook.js and koala library to support Facebook interaction.

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,10 @@
+function fb_login(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ }
