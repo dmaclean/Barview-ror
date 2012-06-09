33af092da9f0f9e96fefb6a09812d36cb708b74f
Dan MacLean <dmaclean82@gmail.com>
2012-06-09 11:54:12 -0400

    Mis-typed name for environment variable representing the Facebook secret key.  Should be FB_SECRET_KEY instead of FB_SECRET.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -2,8 +2,8 @@ class FacebookController < ApplicationController
   # Process Facebook login
   def create
     # generate a new oauth object with your app data and your callback url
-    logger.info("Creating Koala oauth for app #{ ENV['FB_APP_ID'] }  with secret #{ ENV['FB_SECRET'] }")
-	session['oauth'] = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_SECRET'], @base_url + '/facebook/callback')
+    logger.info("Creating Koala oauth for app #{ ENV['FB_APP_ID'] }  with secret #{ ENV['FB_SECRET_KEY'] }")
+	session['oauth'] = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_SECRET_KEY'], @base_url + '/facebook/callback')
     
     # redirect to facebook to get your code
 	redirect_to session['oauth'].url_for_oauth_code()
