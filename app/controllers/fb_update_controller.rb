3f23151d937e84675a0963ec5a9aaa913d596367
Dan MacLean <dmaclean82@gmail.com>
2012-06-25 20:26:23 -0400

    Added a line to save the user_id into the session when a mobile app calls the facebook updater component.  With this in place the barview server now knows about the facebook user.

--- app/controllers/fb_update_controller.rb
+++ app/controllers/fb_update_controller.rb
@@ -8,6 +8,11 @@ class FbUpdateController < ApplicationController
     user = User.new
     user.create_or_update_facebook_data(parsed_json)
     
+    # A call to this service only comes from a newly logged in FB user.  So this is a good
+    # spot to register their user id with the session.
+    fbuser = FbUser.find_by_fb_id(parsed_json["id"])
+    session[:user_id] = fbuser.user_id
+    
     render :text => "OK"
   end
 end
