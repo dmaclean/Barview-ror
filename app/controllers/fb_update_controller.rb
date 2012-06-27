568122fb977c6be5ee085d275cb7bd17113e47e7
Dan MacLean <dmaclean82@gmail.com>
2012-06-27 06:07:20 -0400

    Removed some debugging logger output.

--- app/controllers/fb_update_controller.rb
+++ app/controllers/fb_update_controller.rb
@@ -1,9 +1,6 @@
 class FbUpdateController < ApplicationController
   def create
-  
-    logger.info("JSON request contains #{ params[:json] }")
     parsed_json = ActiveSupport::JSON.decode(params[:json])
-    logger.info("Name #{parsed_json['birthday']}")
     
     user = User.new
     user.create_or_update_facebook_data(parsed_json)
