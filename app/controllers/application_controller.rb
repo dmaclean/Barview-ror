7a41e9df4a036bff86e45109ace19a6ce9e9a990
Dan MacLean <dmaclean82@gmail.com>
2012-07-17 19:55:19 -0400

    Protocol determination is now dynamic based on request.protocol in the construction of @base_url.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < ActionController::Base
   # Provides an opportunity to initialize some helpful site-global variables.
   #############################################################################
   def init_vars
-    @base_url = 'http://' + request.host
+    @base_url = request.protocol + request.host
     if request.port != 80
       @base_url += ':' + request.port.to_s
     end
